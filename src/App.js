b05f63f72307561e700a259b932e4f39620fba21
joshl26 <joshlehman.dev@gmail.com>
2022-12-17 14:17:06 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [people, setPeople] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://swapi.dev/api/people/").then((data) => {
+      console.log(data);
+      setPeople(data.data?.results);
+    });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {people.length ? (
+        <>
+          {people.map((person) => (
+            <div>{person.name}</div>
+          ))}
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 }
